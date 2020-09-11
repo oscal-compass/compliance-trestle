40ab61404e2b9ec347c736153c3691fd19f8143f
Bruno <brunomar@au1.ibm.com>
2020-09-11 18:25:32 +1000

    docs: add examples for split/merge commands
    Add example of catalag being edited via trestle using trestle split and trestle merge commands.

---
+++ test/data/split_merge/step0-merged_catalog/catalogs/mycatalog/next_step.sh
@@ -0,0 +1,12 @@
+### Contextual trestle command
+
+trestle split -e uuid,metadata,groups,back-matter
+
+# OR
+trestle split -e uuid -e metadata -e groups -e back-matter
+
+# OR
+trestle split -e uuid
+trestle split -e metadata
+trestle split -e groups
+trestle split -e back-matter
---
+++ test/data/split_merge/step2-split_metadata_elements/catalogs/mycatalog/metadata/next_step.sh
@@ -0,0 +1,10 @@
+### Contextual trestle command
+
+trestle split -e roles[],responsible-parties{}
+
+# OR
+trestle split -e roles[] -e responsible-parties{}
+
+# OR
+trestle split -e roles[]
+trestle split -e responsible-parties{}
