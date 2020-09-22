0d75653e113ba680b176789d6abcb0bc528fd51c
Bruno <brunomar@au1.ibm.com>
2020-09-22 14:58:11 +1000

    test: update trestle split and merge commands
    - add -f option to trestle split command
    - add -f -d options to trestle merge command
    - explain future behaviour of trestle split and merge
    
    Signed-off-by: Bruno <brunomar@au1.ibm.com>

--- test/data/split_merge/step2-split_metadata_elements/catalogs/mycatalog/metadata/next_step.sh
+++ test/data/split_merge/step2-split_metadata_elements/catalogs/mycatalog/metadata/next_step.sh
@@ -1,10 +1,14 @@
-### Contextual trestle command
+# Splits the properties listed in the -e option that must exist in the file specified in the -f option.
+trestle split -f ./roles.json -e 'roles.*'
+trestle split -f ./responsible-parties.json -e 'responsible-parties.*'
 
-trestle split -e roles[],responsible-parties{}
+# In the near future, trestle split should be smart enough to figure out which json file contains the elemenets you want to split
+# In that case, the -f option would be deprecated and the commands would look like:
+trestle split -e 'roles.*,responsible-parties.*'
 
 # OR
-trestle split -e roles[] -e responsible-parties{}
+trestle split -e 'roles.*' -e 'esponsible-parties.*'
 
 # OR
-trestle split -e roles[]
-trestle split -e responsible-parties{}
+trestle split -e 'roles.*'
+trestle split -e 'responsible-parties.*'
