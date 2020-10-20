fa17318af76daab68f3673243a26711c92ddb44e
Lenin Mehedy <lenin.mehedy@au1.ibm.com>
2020-10-20 18:35:31 +1100

    feature: implement move_dir_to_trash and tests
    Signed-off-by: Lenin Mehedy <lenin.mehedy@au1.ibm.com>

--- tests/trestle/utils/trash_test.py
+++ tests/trestle/utils/trash_test.py
@@ -24,9 +24,34 @@ from tests import test_utils
 from trestle.utils import fs, trash
 
 
+def test_get_trash_dir_path(tmp_dir):
+    """Test get_trash_dir_path method."""
+    tmp_file: pathlib.Path = tmp_dir / 'tmp_file.md'
+    tmp_file.touch()
+
+    data_dir: pathlib.Path = tmp_dir / 'data'
+    fs.ensure_directory(data_dir)
+    readme_file: pathlib.Path = data_dir / 'readme.md'
+    readme_file.touch()
+
+    with pytest.raises(AssertionError):
+        trash.get_trash_file_path(readme_file)
+
+    test_utils.ensure_trestle_config_dir(tmp_dir)
+
+    # trestle root will use the trash root
+    assert trash.get_trash_dir_path(tmp_dir).name == pathlib.Path(trash.TRESTLE_TRASH_DIR).name
+    assert trash.get_trash_dir_path(tmp_dir).parent.name == pathlib.Path(trash.TRESTLE_TRASH_DIR).parent.name
+
+    # any directory under trestle rool will have the trash as the parent
+    assert trash.get_trash_dir_path(data_dir).parent.name == pathlib.Path(trash.TRESTLE_TRASH_DIR).name
+
+
 def test_get_trash_file_path(tmp_dir):
     """Test get_trash_file_path method."""
-    tmp_file = tmp_dir / 'tmp_file.md'
+    tmp_file: pathlib.Path = tmp_dir / 'tmp_file.md'
+    tmp_file.touch()
+
     data_dir: pathlib.Path = tmp_dir / 'data'
     fs.ensure_directory(data_dir)
     readme_file: pathlib.Path = data_dir / 'readme.md'
@@ -38,19 +63,58 @@ def test_get_trash_file_path(tmp_dir):
     test_utils.ensure_trestle_config_dir(tmp_dir)
 
     assert trash.get_trash_file_path(tmp_file) is not None
-    assert trash.get_trash_file_path(tmp_file).parent.name == pathlib.Path(trash.TRESTLE_TRASH_DIR).name
-    assert trash.get_trash_file_path(readme_file).parent.name == 'data'
+    assert trash.get_trash_file_path(tmp_file).parent == trash.get_trash_dir_path(tmp_dir)
+    assert trash.get_trash_file_path(readme_file).parent.name == f'data{trash.TRESTLE_TRASH_EXT}'
 
 
-def test_move_to_trash(tmp_dir):
-    """Test move_to_trash command."""
+def test_move_file_to_trash(tmp_dir):
+    """Test moving file to trash."""
     test_utils.ensure_trestle_config_dir(tmp_dir)
+
+    # trash a file
     data_dir: pathlib.Path = tmp_dir / 'data'
     fs.ensure_directory(data_dir)
     readme_file: pathlib.Path = data_dir / 'readme.md'
     readme_file.touch()
 
     assert not trash.get_trash_file_path(readme_file).exists()
-    trash.move_to_trash(readme_file)
+    trash.move_file_to_trash(readme_file, True)
     assert readme_file.exists() is False
+    assert data_dir.exists()
+    assert trash.get_trash_file_path(readme_file).exists()
+
+    # trash using common tash method
+    readme_file.touch()
+    trash.move_to_trash(readme_file, True)
+    assert readme_file.exists() is False
+    assert data_dir.exists()
+    assert trash.get_trash_dir_path(data_dir).exists()
+    assert trash.get_trash_file_path(readme_file).exists()
+
+
+def test_move_dir_to_trash(tmp_dir):
+    """Test moving whole directory to trash."""
+    test_utils.ensure_trestle_config_dir(tmp_dir)
+
+    # trash whole directory
+    data_dir: pathlib.Path = tmp_dir / 'data'
+    fs.ensure_directory(data_dir)
+    readme_file: pathlib.Path = data_dir / 'readme.md'
+    readme_file.touch()
+
+    assert not trash.get_trash_dir_path(data_dir).exists()
+    assert not trash.get_trash_file_path(readme_file).exists()
+    trash.move_dir_to_trash(data_dir, True)
+    assert data_dir.exists() is False
+    assert readme_file.exists() is False
+    assert trash.get_trash_dir_path(data_dir).exists()
+    assert trash.get_trash_file_path(readme_file).exists()
+
+    # trash whole directory
+    fs.ensure_directory(data_dir)
+    readme_file.touch()
+    trash.move_to_trash(data_dir, True)
+    assert data_dir.exists() is False
+    assert readme_file.exists() is False
+    assert trash.get_trash_dir_path(data_dir).exists()
     assert trash.get_trash_file_path(readme_file).exists()
--- trestle/utils/trash.py
+++ trestle/utils/trash.py
@@ -21,35 +21,76 @@ from shutil import copyfile
 from . import fs
 
 TRESTLE_TRASH_DIR = '.trestle/_trash/'
-TRESTLE_TRASH_FILE_EXT = '.bk'
+TRESTLE_TRASH_EXT = '__bk'
+
+
+def get_trash_dir_path(dir_path: pathlib.Path):
+    """Construct the path to the trashed file."""
+    absolute_path = dir_path.absolute()
+    root_path = fs.get_trestle_project_root(absolute_path)
+    if root_path is None:
+        raise AssertionError(f'Directory path "{absolute_path}" is not in a valid trestle project')
+
+    trestle_trash_path = root_path / TRESTLE_TRASH_DIR
+
+    relative_path = absolute_path.relative_to(str(root_path))
+    if len(relative_path.parts) == 0:
+        trash_dir = trestle_trash_path
+    else:
+        trash_dir = trestle_trash_path / f'{relative_path}{TRESTLE_TRASH_EXT}'
+
+    return trash_dir
 
 
 def get_trash_file_path(file_path: pathlib.Path):
     """Construct the path to the trashed file."""
-    absolute_path = file_path.absolute()
-    root_path = fs.get_trestle_project_root(absolute_path)
-
-    if root_path is None:
-        raise AssertionError(f'File path "{absolute_path}" is not in a valid trestle project')
-
-    trash_dir = root_path / TRESTLE_TRASH_DIR
-
-    trash_file_dir = trash_dir / absolute_path.relative_to(str(root_path)).parent
-    trash_file_path = trash_file_dir / f'{file_path.name}{TRESTLE_TRASH_FILE_EXT}'
+    trash_file_dir = get_trash_dir_path(file_path.parent)
+    trash_file_path = trash_file_dir / f'{file_path.name}{TRESTLE_TRASH_EXT}'
 
     return trash_file_path
 
 
-def move_to_trash(file_path: pathlib.Path, delete_file: bool = True):
+def move_file_to_trash(file_path: pathlib.Path, delete_file: bool = True):
     """Move the specified file to the trash directory.
 
     It overwrites the previous file if exists
     """
     if not file_path.is_file():
         raise AssertionError(f'Specified path "{file_path}" is not a file')
+
     trash_file_path = get_trash_file_path(file_path)
     fs.ensure_directory(trash_file_path.parent)
     copyfile(file_path, trash_file_path)
 
     if delete_file:
         file_path.unlink()
+
+
+def move_dir_to_trash(dir_path: pathlib.Path, delete_all: bool = True):
+    """Move the specified dir to the trash directory.
+
+    It overwrites the previous directory and contents if exists
+    """
+    if not dir_path.is_dir():
+        raise AssertionError(f'Specified path "{dir_path}" is not a dir')
+
+    # move all files/directories under sub_path
+    for item_path in pathlib.Path.iterdir(dir_path):
+        if item_path.is_file():
+            move_file_to_trash(item_path, delete_all)
+        elif item_path.is_dir():
+            move_dir_to_trash(item_path, delete_all)
+
+    if delete_all:
+        dir_path.rmdir()
+
+
+def move_to_trash(content_path: pathlib.Path, delete_content: bool = True):
+    """Move the specified file or directory to the trash directory.
+
+    It overwrites the previous file or directory if exists
+    """
+    if content_path.is_file():
+        return move_file_to_trash(content_path, delete_content)
+    elif content_path.is_dir():
+        return move_dir_to_trash(content_path, delete_content)
