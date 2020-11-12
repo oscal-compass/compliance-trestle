764cdec51fe2d16c68fd2f4b121a5818ca6dd547
Chris Butler <chris@thebutlers.me>
2020-11-12 15:02:35 +1100

    Initial set of trestle task docs.
    Signed-off-by: Chris Butler <chris@thebutlers.me>

---
+++ trestle/tasks/base_task.py
@@ -0,0 +1,45 @@
+# -*- mode:python; coding:utf-8 -*-
+
+# Copyright (c) 2020 IBM Corp. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Trestle tasks base templating."""
+
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class TaskOutcome(Enum):
+    """Enum describing possible task outcomes."""
+
+    SUCCESS = 'success'
+    FAILURE = 'failure'
+    ROLLEDBACK = 'rolledback'
+
+
+class TasksBase(ABC):
+    """Abstract base class for tasks."""
+
+    def __init__(self, config_object):
+        """Initialize task base."""
+        self._config_object = config_object
+
+    @abstractmethod
+    def print_help(self) -> str:
+        """Print the help string."""
+        pass
+
+    @abstractmethod
+    def execute() -> TaskOutcome:
+        """Execute the task including potential rollback."""
+        pass
