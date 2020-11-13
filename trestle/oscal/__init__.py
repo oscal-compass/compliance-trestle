cf7efe7a2540c244f49901905b7a5e29d40566b6
Chris Butler <chris@thebutlers.me>
2020-11-13 16:06:20 +1100

    fix: Improvements in typing and return codes.
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- trestle/oscal/__init__.py
+++ trestle/oscal/__init__.py
@@ -13,3 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+#TODO: Ensure this is automatically updated succusfully.
+OSCAL_VERSION = 'v1.0.0-milestone3'
