a895d15d5b758073e4c244902d7a521cf71ae85e
Jeff Tan <jeffetan@au1.ibm.com>
2020-11-20 17:55:36 +1100

    Testing removal of dict and list elements (not their content).
    Signed-off-by: Jeff Tan <jeffetan@au1.ibm.com>

--- tests/trestle/core/commands/remove_test.py
+++ tests/trestle/core/commands/remove_test.py
@@ -35,40 +35,56 @@ from trestle.oscal.catalog import Catalog
 
 def test_remove(tmp_dir, sample_catalog_minimal):
     """Test RemoveCmd.remove() method for trestle remove: removing Roles."""
-    # minimal catalog, no Roles
+    # 1. Remove responsible-parties
+    # Note: minimal catalog does have responsible-parties but doesn't have Roles.
     file_path = pathlib.Path.joinpath(test_utils.JSON_TEST_DATA_PATH, 'minimal_catalog.json')
-    minimal_catalog = Element(Catalog.oscal_read(file_path))
-#    minimal_catalog_model = Catalog.oscal_read(file_path)
-
-    # minimal catalog with Roles
-    file_path = pathlib.Path.joinpath(test_utils.JSON_TEST_DATA_PATH, 'minimal_catalog_roles.json')
-    catalog_with_roles = Element(Catalog.oscal_read(file_path))
+    catalog_with_responsible_parties = Element(Catalog.oscal_read(file_path))
 
     # minimal catalog with responsible-parties (dict) removed
     file_path = pathlib.Path.joinpath(test_utils.JSON_TEST_DATA_PATH, 'minimal_catalog_no_responsible-parties.json')
     expected_catalog_responsible_parties_removed = Element(Catalog.oscal_read(file_path))
 
-    content_type = FileContentType.JSON
-
-    # catalog_def_dir, catalog_def_file = test_utils.prepare_trestle_project_dir(
-    #     tmp_dir,
-    #     content_type,
-    #     sample_catalog_minimal,
-    #     test_utils.CATALOGS_DIR
-    # )
-
+    ##  Target path for removal:
     element_path = ElementPath('catalog.metadata.responsible-parties')
-    catalog_element = Element(sample_catalog_minimal)
-    expected_remove_action = RemoveAction(catalog_element, element_path)
+    expected_remove_action = RemoveAction(catalog_with_responsible_parties, element_path)
 
-    actual_remove_action, actual_catalog_removed_responsible_parties = RemoveCmd.remove(element_path, Catalog, catalog_element)
+    ## Call remove() method
+    actual_remove_action, actual_catalog_removed_responsible_parties = RemoveCmd.remove(element_path, Catalog, catalog_with_responsible_parties)
 
+    ## 1.1 Assertion about action
     assert expected_remove_action == actual_remove_action
-#    assert expected_catalog_responsible_parties_removed == actual_catalog_removed_responsible_parties
 
     add_plan = Plan()
     add_plan.add_action(actual_remove_action)
     add_plan.simulate()
     add_plan.execute()
 
-    assert catalog_element == actual_catalog_removed_responsible_parties
+    ## 1.2 Assertion about resulting element after removal
+    assert expected_catalog_responsible_parties_removed == actual_catalog_removed_responsible_parties
+
+    # 2. Remove roles
+    # Note: minimal catalog does have responsible-parties but doesn't have Roles.
+    file_path = pathlib.Path.joinpath(test_utils.JSON_TEST_DATA_PATH, 'minimal_catalog.json')
+    catalog_without_roles = Element(Catalog.oscal_read(file_path))
+
+    # minimal catalog with Roles
+    file_path = pathlib.Path.joinpath(test_utils.JSON_TEST_DATA_PATH, 'minimal_catalog_roles.json')
+    catalog_with_roles = Element(Catalog.oscal_read(file_path))
+
+    ##  Target path for removal:
+    element_path = ElementPath('catalog.metadata.roles')
+    expected_remove_action = RemoveAction(catalog_with_roles, element_path)
+
+    ## Call remove() method
+    actual_remove_action, actual_catalog_removed_roles = RemoveCmd.remove(element_path, Catalog, catalog_with_roles)
+
+    ## 2.1 Assertion about action
+    assert expected_remove_action == actual_remove_action
+
+    add_plan = Plan()
+    add_plan.add_action(actual_remove_action)
+    add_plan.simulate()
+    add_plan.execute()
+
+    ## 2.2 Assertion about resulting element after removal
+    assert catalog_without_roles == actual_catalog_removed_roles
