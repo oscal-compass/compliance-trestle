b8dba78072304138696a8fb38529e49da500550c
Vikas <avikas@in.ibm.com>
2021-04-15 13:04:41 +0530

    chnaged benchmark to use transform API

--- scripts/experiments/tanium_ben.py
+++ scripts/experiments/tanium_ben.py
@@ -15,15 +15,13 @@
 # limitations under the License.
 """Script to benchmark tanium to OSCAL conversion."""
 
-import io
-import json
 import logging
 import sys
 import timeit
 from pathlib import Path
 
-from trestle.oscal.assessment_results import Result
-from trestle.utils import tanium
+from trestle.core.base_model import OscalBaseModel
+from trestle.transforms.transformer_singleton import transformer_factory
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -34,25 +32,18 @@ with_json_ser = True
 def tanium_wrapper(data: str) -> str:
     """Convert input tanium data (in string format) to OSCAL result (json serlaized as string)."""
     # process
-    results_mgr = tanium.ResultsMgr()
-    stream = io.StringIO(data)
-    line = stream.readline()
-    while line:
-        jdata = json.loads(line.strip())
-        results_mgr.ingest(jdata)
-        line = stream.readline()
-    json_str = results_mgr.json
+    tanium_tf = transformer_factory.get('tanium')
+    output_oscal = tanium_tf.transform(data)
+    json_str = output_oscal.oscal_serialize_json()
     return json_str
 
 
-def tanium_wrapper_oscal(data: list) -> Result:
-    """Convert input tanium data (in json format) to OSCAL result."""
+def tanium_wrapper_oscal(data: str) -> OscalBaseModel:
+    """Convert input tanium data (in string format) to OSCAL result."""
     # process
-    results_mgr = tanium.ResultsMgr()
-    for line in data:
-        results_mgr.ingest(line)
-
-    return results_mgr.result
+    tanium_tf = transformer_factory.get('tanium')
+    output_oscal = tanium_tf.transform(data)
+    return output_oscal
 
 
 def read_file(in_file: str):
@@ -78,21 +69,9 @@ def main(in_file: str, out_file: str):
     repeat = 3
     logger.info('Num Iter per exp: ' + str(num_iter) + ', Repeat: ' + str(repeat))
 
-    # create json array for input
-    json_data = []
-    stream = io.StringIO(orig_data)
-    line = stream.readline()
-    while line:
-        jdata = json.loads(line.strip())
-        json_data.append(jdata)
-        line = stream.readline()
-
     for n in range(num_exp):
         logger.info('*****Experiment: ' + str(n + 1) + ', Num input records: ' + str(records * (n + 1)))
-        if with_json_ser:
-            data = orig_data * (n + 1)  # with json ser and deser
-        else:
-            data = json_data * (n + 1)  # without json ser and deser
+        data = orig_data * (n + 1)  # with json ser and deser
 
         total_time = 0
         for _i in range(repeat):
