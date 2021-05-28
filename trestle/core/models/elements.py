d17a8f94447494dfb523a75c04a0d06f89d234fe
FrankSuits <frankst@au1.ibm.com>
2021-05-28 12:55:31 +1000

    fix to bug splitting roles and compdefs.  Better handling of lists and dicts in splits
    Signed-off-by: FrankSuits <frankst@au1.ibm.com>

--- trestle/core/models/elements.py
+++ trestle/core/models/elements.py
@@ -221,12 +221,29 @@ class Element:
         self._elem: OscalBaseModel = elem
 
         if wrapper_alias == '' and wrapper_alias != self.IGNORE_WRAPPER_ALIAS:
+            class_name = elem.__class__.__name__
             if utils.is_collection_field_type(elem):
-                raise TrestleError('wrapper_alias is required for a collection type object')
-            else:
-                wrapper_alias = utils.classname_to_alias(elem.__class__.__name__, 'json')
+                class_name = self._get_singular_classname()
+                if class_name is None:
+                    raise TrestleError(
+                        f'wrapper_alias not found for a collection type object: {elem.__class__.__name__}'
+                    )
+            wrapper_alias = utils.classname_to_alias(class_name, 'json')
 
         self._wrapper_alias: str = wrapper_alias
+
+    def _get_singular_classname(self) -> str:
+        """Get the inner class name for list or dict objects."""
+        # this assumes all items in list and all values in dict are same type
+        class_name = None
+        root = getattr(self._elem, '__root__', None)
+        if root is not None:
+            type_str = root.__class__.__name__
+            if type_str == 'list':
+                class_name = self._elem.__root__[0].__class__.__name__
+            elif type_str == 'dict':
+                class_name = list(self._elem.__root__.values())[0].__class__.__name__
+        return class_name
 
     def get(self) -> OscalBaseModel:
         """Return the model object."""
