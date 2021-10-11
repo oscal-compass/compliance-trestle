af260356f88ae2871f9024a848b9c6c38307132c
Chris Butler <chris@thebutlers.me>
2021-10-11 13:45:40 +1100

    chore: running pre-commit that was missing
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- trestle/sample.py
+++ trestle/sample.py
@@ -1,9 +1,12 @@
 # DO NOT COMMIT TO DEVELOP
-
+"""Testing for sonar."""
 import pathlib
 import random
 
+
 def add_a_banana(path: pathlib.Path) -> pathlib.Path:
-    """Here we do some mindless compute then add a number of bananas."""
-    for ii in range(random.randint(1,101)):
-        return path / f'with_{ii}_bananas'
+    """Add a path which is based on random bananas."""
+    cryptogen = random.SystemRandom()
+    for ii in range(cryptogen.randrange(100)):
+        a = ii
+    return path / f'with_{a}_bananas'
