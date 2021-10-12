5dd3f994b618cc2b9bf21d4bca65a1e0d8c58a30
Chris Butler <chris@thebutlers.me>
2021-10-12 19:49:12 +1100

    chore: Initial testing
    Signed-off-by: Chris Butler <chris@thebutlers.me>

---
+++ scripts/ubuntu_saxon_install.sh
@@ -0,0 +1,33 @@
+# Copyright (c) 2020 IBM Corp. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo 'Hello, World!'
+
+echo 'Printing env'
+
+env
+
+echo 'Done'
+# wget -O /tmp/saxon.zip https://www.saxonica.com/saxon-c/libsaxon-HEC-setup64-v1.2.1.zip \
+#   && unzip /tmp/saxon.zip -d /tmp \
+#   && (echo "/opt/saxonica" && cat) | ./tmp/libsaxon-HEC-setup64-v1.2.1 \
+#   && ln -s /opt/saxonica/libsaxonhec.so /usr/lib/libsaxonhec.so \
+#   && ln -s /opt/saxonica/rt /usr/lib/rt
+
+# LD_LIBRARY_PATH=/usr/lib/rt/lib/amd64:$LD_LIBRARY_PATH
+
+# cd /opt/saxonica/Saxon.C.API/python-saxon \
+#   && pip install cython \
+#   && python3 saxon-setup.py build_ext -if
+#   PYTHONPATH=/opt/saxonica/Saxon.C.API/python-saxon:$PYTHONPATH
