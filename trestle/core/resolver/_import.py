ae803e74cda58694a294ccfe1e979f2993626ec8
Alexander Stein <alexander.stein@nist.gov>
2022-02-16 00:58:15 -0500

    feat: Resolve UUID Imports During Profile Resolution, Not Only Explicit Imports (#1023)
    * feat: Resolve import uuid anchor during profile resolution.
    
    Signed-off-by: Alexander Stein <alexander.stein@nist.gov>
    
    * feat: Add profile resolv docs
    
    And of course reach to the two commit minimum for the PR. :-)
    
    Signed-off-by: Alexander Stein <alexander.stein@nist.gov>
    
    * feat: Check for missing resources pre-assignment
    
    Tests wisely indicate that a profile.back_matter, can have a falsey None
    value for resources and there should a conditional assignment test before
    to ensure that.
    
    Signed-off-by: Alexander Stein <alexander.stein@nist.gov>
    
    * feat: Add with @fsuit's feedback.
    
    Signed-off-by: Alexander Stein <alexander.stein@nist.gov>

--- trestle/core/resolver/_import.py
+++ trestle/core/resolver/_import.py
@@ -15,7 +15,7 @@
 
 import logging
 import pathlib
-from typing import Iterator, List, Union
+from typing import Iterator, List, Optional, Union
 
 import trestle.common.const as const
 import trestle.oscal.catalog as cat
@@ -27,6 +27,7 @@ from trestle.core.remote import cache
 from trestle.core.resolver.merge import Merge
 from trestle.core.resolver.modify import Modify
 from trestle.core.resolver.prune import Prune
+from trestle.oscal.common import Resource
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +42,8 @@ class Import(Pipeline.Filter):
         change_prose=False,
         block_adds: bool = False,
         params_format: str = None,
-        param_rep: ParameterRep = ParameterRep.VALUE_OR_LABEL_OR_CHOICES
+        param_rep: ParameterRep = ParameterRep.VALUE_OR_LABEL_OR_CHOICES,
+        resources: Optional[List[Resource]] = None
     ) -> None:
         """Initialize and store trestle root for cache access."""
         self._trestle_root = trestle_root
@@ -50,6 +52,24 @@ class Import(Pipeline.Filter):
         self._change_prose = change_prose
         self._params_format = params_format
         self._param_rep = param_rep
+        self._resources = resources
+
+        if self._import.href[0] == '#':
+            # Specification section on internal reference resolution:
+            # https://pages.nist.gov/OSCAL/concepts/processing/profile-resolution/#d2e300-head
+            try:
+                resource = [r for r in self._resources if r.uuid == self._import.href[1:]][0]
+                self._import.href = [
+                    rlink.href
+                    for rlink in resource.rlinks
+                    if rlink.href.endswith('.json') or rlink.href.endswith('.yaml') or rlink.href.endswith('.yml')
+                ][0]
+
+            except Exception as e:
+                logger.error(f'Profile resolution failed for resource with uuid: {self._import.href}')
+                raise TrestleError(
+                    f'Back matter resource resolution needed for profile import failed with error: {str(e)}'
+                )
 
     def process(self, _=None) -> Iterator[cat.Catalog]:
         """Load href for catalog or profile and yield each import as catalog imported by its distinct pipeline."""
@@ -65,14 +85,16 @@ class Import(Pipeline.Filter):
         else:
             if model_type != const.MODEL_TYPE_PROFILE:
                 raise TrestleError(f'Improper model type {model_type} as profile import.')
+
             profile: prof.Profile = model
+            resources = profile.back_matter.resources if profile.back_matter and profile.back_matter.resources else None
 
             pipelines: List[Pipeline] = []
             logger.debug(
                 f'import pipelines for sub_imports of profile {self._import.href} with title {model.metadata.title}'
             )
             for sub_import in profile.imports:
-                import_filter = Import(self._trestle_root, sub_import)
+                import_filter = Import(self._trestle_root, sub_import, resources=resources)
                 prune_filter = Prune(sub_import, profile)
                 pipeline = Pipeline([import_filter, prune_filter])
                 pipelines.append(pipeline)
