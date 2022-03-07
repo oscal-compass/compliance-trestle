d502cdfa46258b2907f5fb7d40e495438f0e30ff
degenaro <lou.degenaro@gmail.com>
2022-03-07 11:18:00 -0500

    Fix: year is 2022 in code copyright.

--- trestle/tasks/xlsx_helper.py
+++ trestle/tasks/xlsx_helper.py
@@ -1,5 +1,5 @@
 # -*- mode:python; coding:utf-8 -*-
-# Copyright (c) 2021 IBM Corp. All rights reserved.
+# Copyright (c) 2022 IBM Corp. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
