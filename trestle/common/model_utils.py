e2b972290279d2f6f96bdf2f89712a9f4a65f64c
Frank Suits <frankst@au1.ibm.com>
2022-03-16 15:53:54 +1100

    handle last_modified missing
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- trestle/common/model_utils.py
+++ trestle/common/model_utils.py
@@ -600,10 +600,14 @@ class ModelUtils:
     @staticmethod
     def update_timestamp(model: TopLevelOscalModel) -> None:
         """Update the LastModified timestamp in top level model to now."""
-        model.metadata.last_modified.__root__ = datetime.now().astimezone()
+        model.metadata.last_modified = common.LastModified(__root__=datetime.now().astimezone())
 
     @staticmethod
     def model_age(model: TopLevelOscalModel) -> int:
         """Find time in seconds since LastModified timestamp."""
-        dt = datetime.now().astimezone() - model.metadata.last_modified.__root__
-        return dt.seconds
+        # default to one year if no last_modified
+        age_seconds = const.DAY_SECONDS * 365
+        if model.metadata.last_modified:
+            dt = datetime.now().astimezone() - model.metadata.last_modified.__root__
+            age_seconds = dt.seconds
+        return age_seconds
