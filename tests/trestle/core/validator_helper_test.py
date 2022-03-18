55eb7c05dada7fa2adcdd6f9f9fd97fc9149c11d
Frank Suits <frankst@au1.ibm.com>
2022-03-18 13:04:34 +1100

    changed doc string
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- tests/trestle/core/validator_helper_test.py
+++ tests/trestle/core/validator_helper_test.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for models util module."""
+"""Tests for validator helper functionality that was moved to model_utils."""
 
 import pathlib
 from uuid import uuid4
