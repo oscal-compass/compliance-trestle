0524b7309be17dc45ae17da97d8a28df9cbb729e
Frank Suits <frankst@au1.ibm.com>
2022-05-05 17:01:07 +1000

    fixed smell
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- trestle/core/links_validator.py
+++ trestle/core/links_validator.py
@@ -39,44 +39,41 @@ class LinksValidator(Validator):
         returns:
             Always returns True, but gives warning if links and resources are not one-to-one.
         """
-        uuid_strs: List[str] = []
-
         # links have href of form #foo or #uuid
         links_list: List[List[com.Link]] = ModelUtils.find_values_by_name(model, 'links')
-        for links in links_list:
-            for link in links:
-                uuid_strs.append(link.href)
+        uuid_strs = [link.href for links in links_list for link in links]
 
         # prose has uuid refs in markdown form: [foo](#bar) or [foo](#uuid)
         prose_list = ModelUtils.find_values_by_name(model, 'prose')
         for prose in prose_list:
             matches = re.findall(const.MARKDOWN_URL_REGEX, prose)
-            # extract the potential #uuid string from each match
-            for match in matches:
-                uuid_strs.append(match[1])
+            uuid_strs.extend([match[1] for match in matches])
 
-        # now go through all matches and build list of those that are uuids
+        # collect the strings that start with # and are potential uuids
+        uuid_strs = [uuid_str for uuid_str in uuid_strs if uuid_str and uuid_str[0] == '#']
+
+        # go through all matches and build set of those that are uuids
         refs = set()
         for uuid_str in uuid_strs:
-            if uuid_str[0] == '#':
-                uuid_match = re.findall(const.UUID_REGEX, uuid_str[1:])
-                if uuid_match:
-                    refs.add(uuid_match[0])
+            uuid_match = re.findall(const.UUID_REGEX, uuid_str[1:])
+            if uuid_match:
+                refs.add(uuid_match[0])
 
+        # find uuids in backmatter
+        # should check for duplicates
         links = set()
         if model.back_matter and model.back_matter.resources:
-            for res in model.back_matter.resources:
-                links.add(res.uuid)
+            links.update([res.uuid for res in model.back_matter.resources])
 
-        if refs == links:
-            return True
         in_refs = refs.difference(links)
         if in_refs:
             logger.warning(f'Model references {len(refs)} uuids and {len(in_refs)} of them are not in resources.')
             logger.debug(f'Model references {len(in_refs)} uuids not in resources: {in_refs}')
+
         in_links = links.difference(refs)
         if in_links:
             logger.warning(f'Resources have {len(links)} uuids and {len(in_links)} are not referenced by model.')
             logger.debug(f'Resources have {len(in_links)} not in referenced by model: {in_links}')
+
         # This validator is intended just to give warnings, so it always returns True
         return True
