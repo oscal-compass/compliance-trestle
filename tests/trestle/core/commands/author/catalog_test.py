6633d71f5eb7117a2788b2a36e090efd97fd0b28
Frank Suits <frankst@au1.ibm.com>
2022-05-12 17:53:48 +1000

    added test
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- tests/trestle/core/commands/author/catalog_test.py
+++ tests/trestle/core/commands/author/catalog_test.py
@@ -231,6 +231,8 @@ def test_catalog_interface(sample_catalog_rich_controls: cat.Catalog) -> None:
     interface.update_catalog_controls()
     assert interface._catalog.controls[1].controls[0].title == new_title
 
+    assert interface.delete_control_from_catalog('control_d1')
+
 
 def test_catalog_interface_groups() -> None:
     """Test handling of groups of groups in CatalogInterface."""
