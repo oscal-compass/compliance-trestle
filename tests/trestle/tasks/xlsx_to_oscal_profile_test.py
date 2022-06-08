6a1a7fc5ad097f59a55ece428184f4002a98c3d9
degenaro <lou.degenaro@gmail.com>
2022-06-08 14:48:27 -0400

    Fix comment.

--- tests/trestle/tasks/xlsx_to_oscal_profile_test.py
+++ tests/trestle/tasks/xlsx_to_oscal_profile_test.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""XLSX to OSCAL component-definition task tests."""
+"""XLSX to OSCAL profile task tests."""
 
 import configparser
 import os
