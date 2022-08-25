f1e2a9fa0f44db46fd6674be29cbf1b7dd5bf14e
Frank Suits <47203786+fsuits@users.noreply.github.com>
2022-08-25 17:01:31 +1000

    fix: utf8 issue (#1160)
    * fixed utf8 issue
    
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>
    
    * added docs
    
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>
    
    * updated pre-commit
    
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>
    
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- trestle/common/file_utils.py
+++ trestle/common/file_utils.py
@@ -272,17 +272,18 @@ def insert_text_in_file(file_path: pathlib.Path, tag: Optional[str], text: str) 
     Return True on success, False tag not found.
     Text is a string with appropriate \n line endings.
     If tag is none just add at end of file.
+    This will only open file once if tag is not found.
     """
     if not file_path.exists():
         raise TrestleError(f'Test file {file_path} not found.')
     if tag:
         lines: List[str] = []
-        with file_path.open('r') as f:
+        with file_path.open('r', encoding='utf-8') as f:
             lines = f.readlines()
         for ii, line in enumerate(lines):
             if line.find(tag) >= 0:
                 lines.insert(ii + 1, text)
-                with file_path.open('w') as f:
+                with file_path.open('w', encoding='utf-8') as f:
                     f.writelines(lines)
                 return True
     else:
