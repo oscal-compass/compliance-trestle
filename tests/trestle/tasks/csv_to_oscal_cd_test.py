557a7b74fafd6b478c9c1938a287a52e2f9dabb6
degenaro <lou.degenaro@gmail.com>
2022-11-17 07:20:32 -0500

    unit test - no exception leading/training whitespace in Rule Description
    Signed-off-by: degenaro <lou.degenaro@gmail.com>

--- tests/trestle/tasks/csv_to_oscal_cd_test.py
+++ tests/trestle/tasks/csv_to_oscal_cd_test.py
@@ -485,6 +485,19 @@ def test_execute_optional_columns(tmp_path: pathlib.Path):
     _validate3(tmp_path)
 
 
+def test_execute_whitespace(tmp_path: pathlib.Path):
+    """Test execute leading and trailing whitespace."""
+    config = configparser.ConfigParser()
+    config_path = pathlib.Path('tests/data/tasks/csv/test-csv-to-oscal-cd.config')
+    config.read(config_path)
+    section = config['task.csv-to-oscal-cd']
+    section['output-dir'] = str(tmp_path)
+    section['csv-file'] = 'tests/data/spread-sheet/ocp4-whitespace.csv'
+    tgt = csv_to_oscal_cd.CsvToOscalComponentDefinition(section)
+    retval = tgt.execute()
+    assert retval == TaskOutcome.SUCCESS
+
+
 def test_execute_duplicate_rule(tmp_path: pathlib.Path):
     """Test execute duplicate rule."""
     config = configparser.ConfigParser()
