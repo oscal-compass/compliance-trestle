f9caa51b07a1ab3ccbdf959b27e9911c4a23daf6
degenaro <lou.degenaro@gmail.com>
2023-02-06 15:32:27 -0500

    Fix comment.
    Signed-off-by: degenaro <lou.degenaro@gmail.com>

--- tests/trestle/tasks/cis_xlsx_to_oscal_catalog_test.py
+++ tests/trestle/tasks/cis_xlsx_to_oscal_catalog_test.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""cis-xlsx-to--oscal-catalog task tests."""
+"""cis-xlsx-to-oscal-catalog task tests."""
 
 import configparser
 import os
