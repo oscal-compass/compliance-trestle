8b79344a490073b89e5588d6581c316831d9af47
Ekaterina Nikonova <enikonovad@gmail.com>
2023-03-14 10:51:57 +1100

    Add timeout on the get call
    Signed-off-by: Ekaterina Nikonova <enikonovad@gmail.com>

--- trestle/core/remote/cache.py
+++ trestle/core/remote/cache.py
@@ -282,7 +282,7 @@ class HTTPSFetcher(FetcherBase):
             auth = HTTPBasicAuth(self._username, self._password)
 
         try:
-            response = requests.get(self._url, auth=auth, verify=verify)
+            response = requests.get(self._url, auth=auth, verify=verify, timeout=10)
         except Exception as e:
             raise TrestleError(f'Cache update failure to connect via HTTPS: {self._url} ({e})')
 
