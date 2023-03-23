33ea8e807276dcf8a7e4f5bbcfbbf0cfee979f23
Frank Suits <frankst@au1.ibm.com>
2023-03-23 14:26:59 +1100

    boosted test
    Signed-off-by: Frank Suits <frankst@au1.ibm.com>

--- tests/trestle/core/commands/author/ssp_test.py
+++ tests/trestle/core/commands/author/ssp_test.py
@@ -669,6 +669,9 @@ def test_ssp_assemble_no_comps(tmp_trestle_dir: pathlib.Path, capsys) -> None:
 
     assem_ssp, _ = ModelUtils.load_model_for_class(tmp_trestle_dir, ssp_name, ossp.SystemSecurityPlan)
     assert len(assem_ssp.system_implementation.components) == 1
+    by_comp = assem_ssp.control_implementation.implemented_requirements[0].by_components[0]
+    assert by_comp.description == prose_sys
+    assert by_comp.implementation_status.state == 'alternative'
 
     test_utils.replace_line_in_file_after_tag(ac_1_path, 'alternative', '\n### Bad Component\n\n#### Status planned\n')
     assert ssp_assemble._run(args) == 1
