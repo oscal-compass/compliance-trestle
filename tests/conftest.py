220b702ba335f45ef44797e4ebde15d67e4db6c5
Abdulaziz Memesh <abdulazizmemesh@gmail.com>
2024-07-29 15:46:13 -0400

    fix: reimplement clean_tmp() to pass 'make test'

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,20 +43,13 @@ TEST_CONFIG: dict = {}
 def clean_tmp():
     """Remove Trestle workspace in /tmp if there's one."""
     if os.name == 'posix':
+        trestle_dirs = ['.trestle', 'dist', 'catalogs', 'profiles', 'component-definitions', 'system-security-plans', 'assessment-plans', 'assessment-results', 'plan-of-action-and-milestones']
         curr_path = os.getcwd()
         os.chdir('/tmp')
-        try:
-            shutil.rmtree('.trestle')
-            shutil.rmtree('dist')
-            shutil.rmtree('catalogs')
-            shutil.rmtree('profiles')
-            shutil.rmtree('component-definitions')
-            shutil.rmtree('system-security-plans')
-            shutil.rmtree('assessment-plans')
-            shutil.rmtree('assessment-results')
-            shutil.rmtree('plan-of-action-and-milestones')
-        finally:
-            os.chdir(curr_path)
+        for dir in trestle_dirs:
+            if pathlib.Path(dir).is_dir():
+                shutil.rmtree(dir)
+        os.chdir(curr_path)
 
 
 @pytest.fixture(scope='function')
