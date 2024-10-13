8edbf0ead349d10dee6471d9062c37ab27a37dba
Chris Butler <chris.butler@redhat.com>
2024-10-13 11:11:08 +1100

    chore: stashing
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+
+# Init code
+
+def get_package_info(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
