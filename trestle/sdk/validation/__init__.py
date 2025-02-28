31e0e834e986bd492d2fde5f3a9fe289b313887a
Jennifer Power <barnabei.jennifer@gmail.com>
2025-02-27 19:32:01 -0500

    fix: update copyright line in new packages
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- trestle/sdk/validation/__init__.py
+++ trestle/sdk/validation/__init__.py
@@ -1,6 +1,6 @@
 # -*- mode:python; coding:utf-8 -*-
 
-# Copyright (c) 2020 IBM Corp. All rights reserved.
+# Copyright (c) 2025 The OSCAL Compass Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
