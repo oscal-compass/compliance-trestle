256ed8cbfe2a8ea9e1fdae195b6c1c2c9fe0362f
nXtCyberNet <rohantech2005@gmail.com>
2026-03-24 19:15:39 +0530

    updated the seed_url
    Signed-off-by: nXtCyberNet <rohantech2005@gmail.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -5,7 +5,7 @@ pip3 install "$SRC/compliance-trestle"
 
 # Download the real NIST SP800-53 rev5 catalog as the fuzz seed corpus.
 # Using the pinned commit from usnistgov/oscal-content for reproducibility.
-SEED_URL="https://github.com/usnistgov/oscal-content/blob/941c978d14c57379fbf6f7fb388f675067d5bff7/nist.gov/SP800-53/rev5/json/NIST_SP-800-53_rev5_catalog-min.json"
+SEED_URL="https://raw.githubusercontent.com/usnistgov/oscal-content/941c978d14c57379fbf6f7fb388f675067d5bff7/nist.gov/SP800-53/rev5/json/NIST_SP-800-53_rev5_catalog-min.json"
 CORPUS_DIR="$OUT/fuzz_catalog_seed_corpus"
 mkdir -p "$CORPUS_DIR"
 curl -fsSL "$SEED_URL" -o "$CORPUS_DIR/nist_sp800_53_rev5.json"
