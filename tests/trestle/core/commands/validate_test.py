45299998babaa86d5cefa709a9d31ebd3c752bf8
ARNAV RAJ <126798788+Acuspeedster@users.noreply.github.com>
2026-08-31 15:46:17 +0530

    test(validate): enable component-definition port validation coverage (#2195)
    * test(validate): enable component-definition port validation coverage
    
    Signed-off-by: Acuspeedster <arnavrajsingh@gmail.com>
    
    * fix: validation error
    
    Signed-off-by: degenaro <lou.degenaro@gmail.com>
    
    ---------
    
    Signed-off-by: ARNAV RAJ <126798788+Acuspeedster@users.noreply.github.com>
    Signed-off-by: degenaro <lou.degenaro@gmail.com>
    Co-authored-by: Lou DeGenaro <lou.degenaro@gmail.com>
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- tests/trestle/core/commands/validate_test.py
+++ tests/trestle/core/commands/validate_test.py
@@ -505,8 +505,7 @@ def test_validate_component_definition(
     test_utils.execute_command_and_assert(validate_command, 0, monkeypatch)
 
 
-@pytest.mark.skip()
-def xest_validate_component_definition_ports(
+def test_validate_component_definition_ports(
     tmp_trestle_dir: pathlib.Path, testdata_dir: pathlib.Path, monkeypatch: MonkeyPatch
 ) -> None:
     """Test validation of ports in Component Definition."""
@@ -525,7 +524,6 @@ def xest_validate_component_definition_ports(
     test_utils.execute_command_and_assert(validate_command, 0, monkeypatch)
 
 
-@pytest.mark.skip()
 def test_validate_component_definition_ports_invalid(
     tmp_trestle_dir: pathlib.Path, testdata_dir: pathlib.Path, monkeypatch: MonkeyPatch
 ) -> None:
@@ -542,7 +540,7 @@ def test_validate_component_definition_ports_invalid(
 
     shutil.copyfile(spth, tpth)
     validate_command = f'trestle validate -f {tpth}'
-    test_utils.execute_command_and_assert(validate_command, 1, monkeypatch)
+    test_utils.execute_command_and_assert(validate_command, CmdReturnCodes.OSCAL_VALIDATION_ERROR.value, monkeypatch)
 
 
 def test_validate_mapping_simple(
