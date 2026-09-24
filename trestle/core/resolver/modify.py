4424a9e0206bad7f1ddff244937ab8e0d18fd512
degenaro <lou.degenaro@gmail.com>
2026-09-24 09:05:28 -0400

    fix: empty list
    Signed-off-by: degenaro <lou.degenaro@gmail.com>

--- trestle/core/resolver/modify.py
+++ trestle/core/resolver/modify.py
@@ -118,7 +118,7 @@ class Modify(Pipeline.Filter):
             attr_list = items
         else:
             attr_list.extend(items)
-        setattr(part, attr, attr_list)
+        setattr(part, attr, none_if_empty(attr_list))
 
     @staticmethod
     def _add_attr_to_control(control: cat.Control, items: List[OBT], attr: str, position: Optional[str]) -> None:
@@ -128,7 +128,7 @@ class Modify(Pipeline.Filter):
             attr_list = items
         else:
             attr_list.extend(items)
-        setattr(control, attr, attr_list)
+        setattr(control, attr, none_if_empty(attr_list))
 
     @staticmethod
     def _get_or_create_trestle_part(control: cat.Control, add: prof.Add) -> common.Part:
@@ -138,8 +138,9 @@ class Modify(Pipeline.Filter):
             trestle_part = common.Part(
                 id=TRESTLE_INHERITED_PROPS_TRACKER, name=TRESTLE_INHERITED_PROPS_TRACKER, props=add.props
             )
-            control.parts = as_list(control.parts)
-            control.parts.append(trestle_part)
+            existing_parts = as_list(control.parts)
+            existing_parts.append(trestle_part)
+            control.parts = existing_parts
             trestle_part = control.parts[-1]
         return trestle_part
 
@@ -181,7 +182,6 @@ class Modify(Pipeline.Filter):
     @staticmethod
     def _add_to_control(control: cat.Control, add: prof.Add) -> None:
         """First step in applying Add to control."""
-        control.parts = as_list(control.parts)
         if add.by_id is None or add.by_id == control.id:
             # add contents will be added to the control directly and with no recursion
             for attr in ['params', 'props', 'parts', 'links']:
@@ -189,11 +189,12 @@ class Modify(Pipeline.Filter):
                 if add_list:
                     Modify._add_attr_to_control(control, add_list[:], attr, add.position)
         else:
-            # this is only called if by_id is not None
-            if not Modify._add_to_parts(control.parts, add):
+            # this is only called if add.by_id is not None
+            parts = as_list(control.parts)
+            if not Modify._add_to_parts(parts, add):
                 logger.warning(f'Could not find id for add in control {control.id}: {add.by_id}')
+            control.parts = none_if_empty(parts)
         Modify._add_to_trestle_props(control, add)
-        control.parts = none_if_empty(control.parts)
 
     @staticmethod
     def _set_overwrite_items(param: common.Parameter, set_param: prof.SetParameter) -> None:
@@ -264,8 +265,8 @@ class Modify(Pipeline.Filter):
         control = self._catalog_interface.get_control_by_param_id(set_param.param_id)
         loose_param = False
         if control:
-            control.params = as_list(control.params)
-            param_ids = [param.id for param in control.params]
+            params = as_list(control.params)
+            param_ids = [param.id for param in params]
             if set_param.param_id not in param_ids:
                 raise TrestleNotFoundError(f'Param id {set_param.param_id} not found in control {control.id}')
             index = param_ids.index(set_param.param_id)
